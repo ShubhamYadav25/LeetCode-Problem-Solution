bbe7c6671fb50d07b292f53cce1bb3db86b16509
ShubhamYadav25 <63467701+ShubhamYadav25@users.noreply.github.com>
2025-04-11 00:47:30 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (26.74%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
+/**
+ More optimized to my prev soln
+*/
     vector<int> finalPrices(vector<int>& nums) {
         
     int n = nums.size();
-    vector<int> result(n, -1);
     stack<int> st;
 
     for (int i = 0; i < n; i++) {
         while (!st.empty() && nums[i] <= nums[st.top()]) {
-            result[st.top()] = nums[i];
+            nums[st.top()] -= nums[i];
             st.pop();
         }
         st.push(i);
     }
     
-    vector<int> res = nums;
-    for(int i = 0; i < n; i++){
-        if(result[i] == -1) continue;
-        res[i] = nums[i] - result[i];
-    }
-
-    return res;
-
+    return nums;
+   
     }
 };
