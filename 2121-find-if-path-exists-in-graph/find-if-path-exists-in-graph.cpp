4fba4bf900315669a5c22fd2b64193ecfbee3d21
ShubhamYadav25 <63467701+ShubhamYadav25@users.noreply.github.com>
2025-06-27 00:02:53 +0530

    Time: 230 ms (86.47%) | Memory: 281.6 MB (82.32%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -14,14 +14,29 @@ public:
     }
 
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        if(n == 1) return true;
         vector<vector<int>> adj(n);
-
-        for(auto i : edges){
-            adj[i[0]].push_back(i[1]);
-            adj[i[1]].push_back(i[0]);
+        for (auto &e : edges) {
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
         }
-        vector<bool> vis(n);
-        return dfs(adj, source, destination, vis);
+
+        vector<bool> visited(n, false);
+        queue<int> q;
+        q.push(source);
+        visited[source] = true;
+
+        while (!q.empty()) {
+            int node = q.front(); q.pop();
+            if (node == destination) return true;
+
+            for (int neighbor : adj[node]) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    q.push(neighbor);
+                }
+            }
+        }
+
+        return false;
     }
 };
