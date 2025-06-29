e2c21737cffe1314f9c1fae11b3023c1ae6858c7
ShubhamYadav25 <63467701+ShubhamYadav25@users.noreply.github.com>
2025-06-29 14:24:08 +0530

    Time: 20 ms (94.32%) | Memory: 18 MB (45.60%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,23 +1,30 @@
 class DSU {
     vector<int> parent;
     vector<int> rank;
+    int count;
 
 public:
-    DSU(int size) {
-        parent.resize(size, -1);
-        rank.resize(size, 0);    
-    } 
+    DSU(int size) : parent(size), rank(size, 0), count(0) {
+        iota(parent.begin(), parent.end(), 0);
+    }
 
-    int Find(int v) {
-        if (parent[v] == -1)
-            return v;
-        return parent[v] = Find(parent[v]);
-    } 
+    void setCount(int c) {
+        count = c;
+    }
+
+    int getCount() const {
+        return count;
+    }
+
+    int Find(int x) {
+        if (parent[x] != x)
+            parent[x] = Find(parent[x]);
+        return parent[x];
+    }
 
     void Union(int x, int y) {
         int rootX = Find(x);
         int rootY = Find(y);
-
         if (rootX == rootY) return;
 
         if (rank[rootX] < rank[rootY]) {
@@ -28,10 +35,8 @@ public:
             parent[rootY] = rootX;
             rank[rootX]++;
         }
-    }
 
-    vector<int>& getParents() {
-        return parent;
+        count--;  // Only decrement for a successful union
     }
 };
 
@@ -42,49 +47,43 @@ public:
 
         int rows = grid.size();
         int cols = grid[0].size();
-        
-        // 1d matrix using ids = i * colm + j
         DSU dsu(rows * cols);
 
-        vector<vector<int>> directions = {{0,1}, {1,0}, {-1,0}, {0,-1}};
+        auto getId = [&](int i, int j) {
+            return i * cols + j;
+        };
 
-        // First we do union of each cell to its neighbour
+        int landCount = 0;
+
         for (int i = 0; i < rows; ++i) {
             for (int j = 0; j < cols; ++j) {
                 if (grid[i][j] == '1') {
+                    landCount++;
+                }
+            }
+        }
 
-                    // 1d matrix ids
-                    int current = i * cols + j;
+        dsu.setCount(landCount); 
 
+        // Only right and down to avoid duplicates
+        vector<pair<int, int>> directions = {{0,1}, {1,0}};  
+
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (grid[i][j] == '1') {
+                    int currId = getId(i, j);
                     for (auto& dir : directions) {
-                        int ni = i + dir[0];
-                        int nj = j + dir[1];
-
-                        // boundry limits
+                        int ni = i + dir.first;
+                        int nj = j + dir.second;
                         if (ni >= 0 && ni < rows && nj >= 0 && nj < cols && grid[ni][nj] == '1') {
-                            int neighbor = ni * cols + nj;
-                            dsu.Union(current, neighbor);
+                            int neighborId = getId(ni, nj);
+                            dsu.Union(currId, neighborId);
                         }
                     }
                 }
             }
         }
 
-        // Now we have sets ready, to find island 
-        // we iterate over every cell if their belong to parent then no issue (one island)
-        // if they belong to separate set the parent will be different == diffetent island we increase count
-        
-        // I think we can optimize it more by using visited array (will check later)
-        unordered_set<int> uniqueIslands;
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < cols; ++j) {
-                if (grid[i][j] == '1') {
-                    int id = i * cols + j;
-                    uniqueIslands.insert(dsu.Find(id));
-                }
-            }
-        }
-
-        return uniqueIslands.size();
+        return dsu.getCount();
     }
 };
