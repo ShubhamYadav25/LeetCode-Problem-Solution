5b840d55a0f630542dd3906f2a5469b4b049992e
ShubhamYadav25 <63467701+ShubhamYadav25@users.noreply.github.com>
2025-06-29 13:42:20 +0530

    Time: 24 ms (79.37%) | Memory: 18.5 MB (43.10%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,37 +1,90 @@
-class Solution {
+class DSU {
+    vector<int> parent;
+    vector<int> rank;
+
 public:
+    DSU(int size) {
+        parent.resize(size, -1);
+        rank.resize(size, 0);    
+    } 
 
-    void dfs(vector<vector<char>>& grid, int n, int m, int i, int j, vector<vector<bool>>& v){
-        
-        // boundry
-        if(i < 0 || i >= n || j < 0 || j >= m || (grid[i][j] == '0') ||  v[i][j]){
-            return;
+    int Find(int v) {
+        if (parent[v] == -1)
+            return v;
+        return parent[v] = Find(parent[v]);
+    } 
+
+    void Union(int x, int y) {
+        int rootX = Find(x);
+        int rootY = Find(y);
+
+        if (rootX == rootY) return;
+
+        if (rank[rootX] < rank[rootY]) {
+            parent[rootX] = rootY;
+        } else if (rank[rootX] > rank[rootY]) {
+            parent[rootY] = rootX;
+        } else {
+            parent[rootY] = rootX;
+            rank[rootX]++;
         }
-
-        v[i][j] = true;
-
-        // apply dfs in four direction
-        dfs(grid, n, m , i - 1 , j, v);
-        dfs(grid, n, m , i + 1 , j, v);
-        dfs(grid, n, m , i , j -1 , v);
-        dfs(grid, n, m , i , j + 1, v);
     }
 
+    vector<int>& getParents() {
+        return parent;
+    }
+};
 
+class Solution {
+public:
     int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<vector<bool>> v(n, vector<bool>(m , false));
-        int island = 0;
-        for(int i=0; i<n;i++){
-            for(int j = 0; j< m;j++){
-                if(grid[i][j] != '0' && !v[i][j]){
-                    island++;
-                    dfs(grid, n, m, i, j, v);
+        if (grid.empty()) return 0;
+
+        int rows = grid.size();
+        int cols = grid[0].size();
+        
+        // 1d matrix using ids = i * colm + j
+        DSU dsu(rows * cols);
+
+        vector<vector<int>> directions = {{0,1}, {1,0}, {-1,0}, {0,-1}};
+
+        // First we do union of each cell to its neighbour
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (grid[i][j] == '1') {
+
+                    // 1d matrix ids
+                    int current = i * cols + j;
+
+                    for (auto& dir : directions) {
+                        int ni = i + dir[0];
+                        int nj = j + dir[1];
+
+                        // boundry limits
+                        if (ni >= 0 && ni < rows && nj >= 0 && nj < cols && grid[ni][nj] == '1') {
+                            int neighbor = ni * cols + nj;
+                            dsu.Union(current, neighbor);
+                        }
+                    }
                 }
             }
         }
 
-        return island;
+        // Now we have sets ready, to find island 
+        // we iterate over every cell if their belong to parent then no issue (one island)
+        // if they belong to separate set the parent will be different == diffetent island we increase count
+        
+        // I think we can optimize it more by using visited array (will check later)
+        unordered_set<int> uniqueIslands;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (grid[i][j] == '1') {
+                    int id = i * cols + j;
+                    uniqueIslands.insert(dsu.Find(id));
+                }
+            }
+        }
+
+        return uniqueIslands.size();
     }
 };
