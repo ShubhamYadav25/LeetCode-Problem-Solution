cbe12182bab473699eabfd0e80f1e016433d1bf0
ShubhamYadav25 <63467701+ShubhamYadav25@users.noreply.github.com>
2025-06-30 18:12:11 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (75.14%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,24 +1,55 @@
+class DSU {
+    vector<int> parent;
+    vector<int> rank;
+    int count;
+
+public:
+    DSU(int size) : parent(size), rank(size, 0), count(size) {
+        iota(parent.begin(), parent.end(), 0);
+    }
+
+    int getCount() const {
+        return count;
+    }
+
+    int Find(int x) {
+        if (parent[x] != x)
+            parent[x] = Find(parent[x]);
+        return parent[x];
+    }
+
+    void Union(int x, int y) {
+        int rootX = Find(x);
+        int rootY = Find(y);
+        if (rootX == rootY) return;
+
+        if (rank[rootX] < rank[rootY]) {
+            parent[rootX] = rootY;
+        } else if (rank[rootX] > rank[rootY]) {
+            parent[rootY] = rootX;
+        } else {
+            parent[rootY] = rootX;
+            rank[rootX]++;
+        }
+
+        count--;
+    }
+};
+
 class Solution {
 public:
-  void dfs(vector<vector<int>>& isConnected, vector<bool>& visited, int city) {
-        visited[city] = true;
-        for(int i = 0; i < isConnected.size(); i++) {
-            if(isConnected[city][i] == 1 && !visited[i]) {
-                dfs(isConnected, visited, i);
-            }
-        }
-    }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<bool> visited(n, false);
-        int provinces = 0;
-        
-        for(int i = 0; i < n; i++) {
-            if(!visited[i]) {
-                provinces++;
-                dfs(isConnected, visited, i);
+        DSU dsu(n);
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (isConnected[i][j] == 1) {
+                    dsu.Union(i, j);
+                }
             }
         }
-        return provinces;
+
+        return dsu.getCount();
     }
 };
