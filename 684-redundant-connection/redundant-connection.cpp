a11b1bc405ba05c355b7007f6c8a1f64a92ddc0a
ShubhamYadav25 <63467701+ShubhamYadav25@users.noreply.github.com>
2025-07-01 23:59:14 +0530

    Time: 8 ms (13.37%) | Memory: 14.8 MB (13.90%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,45 +1,38 @@
-class DSU {
-   vector<int> parent;
-   vector<int> rank;
-
-public:
-
-    DSU(int size) : parent(size), rank(size, 0) {
-        iota(parent.begin(), parent.end(), 0);
-    }
-
-   int Find(int x) {
-        if (parent[x] != x)
-            parent[x] = Find(parent[x]);
-        return parent[x];
-    }
-
-    void Union(int x, int y) {
-        int rootX = Find(x);
-        int rootY = Find(y);
-
-        if (rank[rootX] < rank[rootY]) {
-            parent[rootX] = rootY;
-        } else if (rank[rootX] > rank[rootY]) {
-            parent[rootY] = rootX;
-        } else {  
-            parent[rootY] = rootX;
-            rank[rootX]++;
-        }
-    }
-};
-
 class Solution {
 public:
-    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        int n = edges.size();
-        DSU dsu(n+1);
+    bool isConnected(int start, int end, vector<vector<int>> &graph, vector<bool> &visited) {
+        if (start == end) return true;
 
-        for(auto v : edges){
-            if(dsu.Find(v[0]) == dsu.Find(v[1])) {
-                return v;
+        visited[start] = true;
+
+        for (int neighbor : graph[start]) {
+            if (!visited[neighbor]) {
+                if (isConnected(neighbor, end, graph, visited)) {
+                    return true;
+                }
             }
-            dsu.Union(v[0], v[1]);
+        }
+
+        return false;
+    }
+
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();  
+        vector<vector<int>> graph(n + 1);  
+
+        for (auto edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            vector<bool> visited(n + 1, false);
+            if (!graph[u].empty() && !graph[v].empty()) {
+                if (isConnected(u, v, graph, visited)) {
+                    return edge;
+                }
+            }
+
+            graph[u].push_back(v);
+            graph[v].push_back(u);
         }
 
         return {};
