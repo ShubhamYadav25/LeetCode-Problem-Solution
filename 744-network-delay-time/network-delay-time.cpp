4f77dc8f5ff36ea28f5aa369b393e9b864589ba0
ShubhamYadav25 <63467701+ShubhamYadav25@users.noreply.github.com>
2025-07-08 01:20:10 +0530

    Time: 66 ms (98.28%) | Memory: 44.2 MB (89.23%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -2,39 +2,33 @@ class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         
-    vector<vector<pair<int, int>>> adj(n + 1);
-    
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    vector<pair<int, int>> adj[101];  
     for (const auto& v : times) {
         adj[v[0]].emplace_back(v[1], v[2]);
     }
 
-    vector<int> distance(n + 1, INT_MAX);
-    distance[k] = 0;
+    vector<int> dist(n + 1, INT_MAX);
+    dist[k] = 0;
 
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
     pq.emplace(0, k);
 
     while (!pq.empty()) {
-        auto [currDist, node] = pq.top();
-        pq.pop();
-
-        if (currDist > distance[node]) continue; 
-
-        for (const auto& [neighbor, weight] : adj[node]) {
-            if (distance[neighbor] > currDist + weight) {
-                distance[neighbor] = currDist + weight;
-                pq.emplace(distance[neighbor], neighbor);
+        auto [d, node] = pq.top(); pq.pop();
+        if (d > dist[node]) continue;
+        for (const auto& [nei, weight] : adj[node]) {
+            if (dist[nei] > d + weight) {
+                dist[nei] = d + weight;
+                pq.emplace(dist[nei], nei);
             }
         }
     }
 
-    int maxDist = 0;
-    for (int i = 1; i <= n; ++i) {
-        if (distance[i] == INT_MAX) return -1;
-        maxDist = max(maxDist, distance[i]);
-    }
-
-    return maxDist;
+    int ans = *max_element(dist.begin() + 1, dist.begin() + n + 1);
+    return ans == INT_MAX ? -1 : ans;
 
     }
 };
