b5ad846b2ae8912646c4ea34e6278e48c14ee181
ShubhamYadav25 <63467701+ShubhamYadav25@users.noreply.github.com>
2025-07-12 09:29:13 +0530

    Time: 25 ms (22.52%) | Memory: 20.6 MB (16.16%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,41 +1,48 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        const int INF = 1e9;
-        vector<vector<int>> dist(n, vector<int>(n, INF));
-        for (int i = 0; i < n; ++i)
-            dist[i][i] = 0;
+        vector<vector<pair<int, int>>> graph(n); 
 
         for (auto& edge : edges) {
             int u = edge[0], v = edge[1], w = edge[2];
-            dist[u][v] = w;
-            dist[v][u] = w;
-        }
-
-        for (int k = 0; k < n; ++k) {
-            for (int i = 0; i < n; ++i) {
-                for (int j = 0; j < n; ++j) {
-                    if (dist[i][k] < INF && dist[k][j] < INF) {
-                        dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
-                    }
-                }
-            }
+            graph[u].push_back({v, w});
+            graph[v].push_back({u, w});
         }
 
         int result = -1;
         int minReachable = n + 1;
 
-        for (int i = 0; i < n; ++i) {
+        for (int start = 0; start < n; ++start) {
+            vector<int> dist(n, INT_MAX);
+            dist[start] = 0;
+
+            priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
+            pq.push({0, start});
+
+            while (!pq.empty()) {
+                auto [currDist, node] = pq.top(); pq.pop();
+
+                if (currDist > dist[node]) continue;
+
+                for (auto& [neighbor, weight] : graph[node]) {
+                    int newDist = currDist + weight;
+                    if (newDist < dist[neighbor]) {
+                        dist[neighbor] = newDist;
+                        pq.push({newDist, neighbor});
+                    }
+                }
+            }
+
             int count = 0;
-            for (int j = 0; j < n; ++j) {
-                if (i != j && dist[i][j] <= distanceThreshold) {
+            for (int i = 0; i < n; ++i) {
+                if (i != start && dist[i] <= distanceThreshold) {
                     count++;
                 }
             }
 
             if (count <= minReachable) {
                 minReachable = count;
-                result = i;
+                result = start;
             }
         }
 
