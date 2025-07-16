94b2d82b32857ea6c49f089978602907bd86c88d
ShubhamYadav25 <63467701+ShubhamYadav25@users.noreply.github.com>
2025-07-16 23:16:54 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (87.62%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+   int dp[47];
+    // memset(dp,47,0);
+    int climbStairs(int n) {
+        if(n==0){
+            return 1;
+        }
+        if(n<0){
+            return 0;
+        }
+        
+        if(dp[n]){
+            return dp[n];
+        }
+        
+        return dp[n]=climbStairs(n-1) + climbStairs(n-2);
+    }
+};
