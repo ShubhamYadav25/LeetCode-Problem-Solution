1ec8500176a90e3067b8ee7429da2e027c4f05de
ShubhamYadav25 <63467701+ShubhamYadav25@users.noreply.github.com>
2025-07-18 01:08:36 +0530

    Time: 75 ms (84.39%) | Memory: 13.5 MB (87.42%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-     bool canPartition(vector<int>& nums) {
+    bool canPartition(vector<int>& nums) {
         int total = 0;
         for (int n : nums) total += n;
-        if (total % 2 != 0) return false; 
+        if (total % 2 != 0) return false;
 
         int target = total / 2;
-        vector<vector<int>> memo(nums.size(), vector<int>(target + 1, -1));
-        return canMakeSum(nums, 0, target, memo);
-    }
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;
 
-    bool canMakeSum(vector<int>& nums, int i, int target, vector<vector<int>>& memo) {
-        if (target == 0) return true;    
-        if (i >= nums.size() || target < 0) return false; 
-
-        if (memo[i][target] != -1) {
-            return memo[i][target];
+        for (int num : nums) {
+            for (int i = target; i >= num; --i) {
+                // If I could already make sum i, keep it as true 
+                // OR
+                // If I could make sum i - num before, then by adding num, I can now make sum i.
+                dp[i] = dp[i] || dp[i - num];
+            }
         }
-
-        // Pick or leave the current number
-        bool pick = canMakeSum(nums, i + 1, target - nums[i], memo);
-        bool leave = canMakeSum(nums, i + 1, target, memo);
-
-        memo[i][target] = pick || leave;    
-        return memo[i][target];
+        return dp[target];
     }
 };
