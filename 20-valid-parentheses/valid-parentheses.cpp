b5e08e21fc06525d6b01b0b7c02254f9c8220269
ShubhamYadav25 <63467701+ShubhamYadav25@users.noreply.github.com>
2025-07-26 14:23:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (60.77%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,25 +2,20 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-        unordered_map<char, char> bracketPair = {
-            {')', '('},
-            {']', '['},
-            {'}', '{'}
-        };
-
         for (char ch : s) {
-            // If it's a closing bracket
-            if (bracketPair.count(ch)) {
-                if (st.empty() || st.top() != bracketPair[ch]) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else {
+                if (st.empty()) return false;
+                char top = st.top();
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
                     return false;
                 }
                 st.pop();
-            } else {
-                // It's an opening bracket
-                st.push(ch);
             }
         }
-
         return st.empty();
     }
 };
