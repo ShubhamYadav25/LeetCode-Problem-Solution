78504694ed56b7859d5f8a338308e295a388e9dd
ShubhamYadav25 <63467701+ShubhamYadav25@users.noreply.github.com>
2025-07-31 01:02:14 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (64.06%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
 
-    bool jump(vector<int>& nums, int i, vector<int>& memo){
-
-        if (i >= nums.size() - 1) return true;
-        if (memo[i] != -1) return memo[i];
-
-        for (int step = 1; step <= nums[i]; ++step) {
-            if (jump(nums, i + step, memo)) {
-                memo[i] = 1;
-                return true;
-            }
-        }
-
-        memo[i] = 0;
-        return false;
-    }
-
     bool canJump(vector<int>& nums) {
-        vector<int> memo(nums.size(), -1);
-        return jump(nums, 0, memo);
+        int farthest = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i > farthest) return false; // Can't reach this index
+            farthest = max(farthest, i + nums[i]);
+        }
+        return true;
     }
 };
