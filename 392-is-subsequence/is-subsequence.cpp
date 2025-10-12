bdf2b6697b66a395f6d1a8ace1f0eba93e52a640
ShubhamYadav25 <63467701+ShubhamYadav25@users.noreply.github.com>
2025-10-12 12:05:15 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (6.52%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,17 +1,35 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int i=0, j=0;
+        // Every entry in pos tells you where that letter occurs in t.
+        /**
+            pos[0] ('a') = [0]
+            pos[1] ('b') = [2]
+            pos[2] ('c') = [5]
+            pos[3] ('d') = [4]
+            pos[4] ('e') = []
+            pos[5] ('f') = []
+            pos[6] ('g') = [3]
+            pos[7] ('h') = [1]
+            pos[8] ('i') = []
+        */
+        vector<vector<int>> pos(26);
+        for (int i = 0; i < t.size(); ++i)
+            pos[t[i] - 'a'].push_back(i);
 
-        while(j < t.length() && i < s.length()){
-            
-            if(s[i] == t[j]){
-                cout<< i << " "<<j<<"\n";
-                i++;
-            }
+        int prev = -1; // previous matched index in t
+        for (char c : s) {
+            auto& vec = pos[c - 'a'];
+            if (vec.empty())
+                return false;
 
-            j++;
+            // find smallest index > prev
+            auto it = upper_bound(vec.begin(), vec.end(), prev);
+            if (it == vec.end())
+                return false;
+
+            prev = *it;
         }
-        return i == s.length();
+        return true;
     }
 };
