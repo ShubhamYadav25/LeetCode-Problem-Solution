7c6013220e8d800be720771b70a85a4d12082ea2
ShubhamYadav25 <63467701+ShubhamYadav25@users.noreply.github.com>
2025-12-17 20:03:38 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (63.24%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> path;  // rooms (arrangement), basically if size =0 then room A
+                       // selected, size = 1 room b selected
+    vector<bool> used; // available items
+    void permutation(vector<int>& nums) {
+
+        // if path size = 0, basically it mean it is Room A is processing
+        // can say if path size == nums.size() => all rooms filled
+        if (path.size() == nums.size()) {
+            ans.push_back(path);
+            return;
+        }
+
+        // now task is too, try placing all the items in selected ROOM for now
+        // say ROOM A can say try placing all items in the current room
+        for (int i = 0; i < nums.size(); i++) {
+
+            // what is it saying, it mean if item already used in some previous
+            // room, skip
+            if (used[i])
+                continue;
+
+            if (i > 0 && nums[i] == nums[i-1] && !used[i-1])
+                continue;
+
+            // if not used, place in current room
+            used[i] = true;
+
+            path.push_back((nums[i]));
+
+            // now size of path is 1 so second room B selected and process this
+            // same process for B
+            permutation(nums);
+
+            // undo before trying next item in this room
+            path.pop_back();
+            used[i] = false;
+        }
+    }
+
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        used.assign(nums.size(), false);
+        permutation(nums);
+        return ans;
+    }
+};
