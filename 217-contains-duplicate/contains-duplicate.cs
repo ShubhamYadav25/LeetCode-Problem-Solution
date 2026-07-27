76105cd4276da66d1ef287a0593e515b2de50a08
ShubhamYadav25 <63467701+ShubhamYadav25@users.noreply.github.com>
2026-07-27 23:07:00 +0530

    Time: 14 ms (62.66%) | Memory: 66 MB (79.75%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cs
+++ 217-contains-duplicate/contains-duplicate.cs
@@ -1,16 +1,6 @@
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
-        var d = new Dictionary<int, int>();
-        
-        for(var i = 0; i < nums.Length; i++){
-             if(d.ContainsKey(nums[i])){
-                 return true;
-             }
+            return nums.Distinct().Count() != nums.Length;
 
-             d.Add(nums[i], 0);
-        }
-        
-        return false;
-        
     }
 }
